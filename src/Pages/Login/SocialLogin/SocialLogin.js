da44e076dcfa28e97e65c868692f9a6fe00a78ce
abir14016 <abir14016@gmail.com>
2022-06-25 03:04:38 +0600

    github login implemented

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -2,13 +2,17 @@ import React from 'react';
 import google from '../../../images/social/google-logo.png';
 import github from '../../../images/social/github-logo.png';
 import './SocialLogin.css';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 
 const SocialLogin = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-    if (user) {
-        console.log(user);
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+    const [signInWithGithub, githubUser, githubLoading, gitubError] = useSignInWithGithub(auth);
+    if (googleUser || githubUser) {
+        console.log(googleUser || githubUser);
+    }
+    if (googleError || gitubError) {
+        console.log(googleError || gitubError)
     }
     return (
         <div>
@@ -18,15 +22,22 @@ const SocialLogin = () => {
                 <div className="col"><hr /></div>
             </div>
             <p className='text-center fw-bold'>continue with</p>
-            <div className='social-logo-container d-flex justify-content-center py-2 bg-light'>
-                <button
-                    onClick={() => signInWithGoogle()}
-                    className='border-0 bg-transparent social-button'>
-                    <img src={google} alt="google" />
-                </button>
-                <button className='border-0 bg-transparent social-button'>
-                    <img src={github} alt="google" />
-                </button>
+            <div className='social-logo-container py-2 bg-light'>
+                <div className='d-flex justify-content-center'>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        className='border-0 bg-transparent social-button'>
+                        <img src={google} alt="google" />
+                    </button>
+                    <button
+                        onClick={() => signInWithGithub()}
+                        className='border-0 bg-transparent social-button'>
+                        <img src={github} alt="google" />
+                    </button>
+                </div>
+                {
+                    (googleLoading || githubLoading) && <h6 className='text-center text-warning fw-bold small-text pt-1'>Loading...</h6>
+                }
             </div>
         </div>
     );
